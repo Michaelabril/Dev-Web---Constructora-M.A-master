ef7d5090aa2a0d71b32bf2750f121aed1e7c5da3
Michael Abril <maiicolabriil@hotmail.com>
2019-05-09 12:48:25 -0500

    deploy web page

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
+
 const express = require('express');
 const path = require('path');
 const app = express();
 
+
 //settings
-app.set('port', 8085);
-app.set('views', path.join(__dirname,'views'));
+app.set('port', 8080);
+app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
-app.set ('view engine', 'ejs');
+app.set('view engine', 'ejs');
 
 
 // middlewares
@@ -17,9 +19,9 @@ app.use(require('./routes/index.js'));
 
 
 //static files
-app.use(express.static(path.join(__dirname,'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 //listening server
-app.listen(app.get('port'), ()=> {
+app.listen(app.get('port'), () => {
   console.log('server on port', app.get('port'));
 });
