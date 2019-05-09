7695b31635a55dd34d726bd5ce9276310adc65c2
Michael Abril <maiicolabriil@hotmail.com>
2019-05-09 13:01:55 -0500

    Resolved problem

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const app = express();
 
 
 //settings
-app.set('port', 8080);
+app.set('port', process.env.PORT || 8080);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
