88c6a5b148acce9afc521a1123ae11e63f2f1289
Michael Abril <maiicolabriil@hotmail.com>
2019-05-10 10:59:00 -0500

    Indentacion completa

--- src/index.js
+++ src/index.js
@@ -1,27 +1,22 @@
-
 const express = require('express');
 const path = require('path');
 const app = express();
 
-
 //settings
-app.set('port', process.env.PORT || 8080);
+app.set('port', process.env.PORT || 8082);
 app.set('views', path.join(__dirname, 'views'));
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'ejs');
-
 
 // middlewares
 
 //routes
 app.use(require('./routes/index.js'));
 
-
-
 //static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 //listening server
 app.listen(app.get('port'), () => {
-  console.log('server on port', app.get('port'));
+    console.log('server on port', app.get('port'));
 });
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const router = express.Router();
 
-
 //route view page path
-router.get('/', (req,res) => {
-  res.render('index.html', {tile: 'ConstructoraMA'});
+router.get('/', (req, res) => {
+    res.render('index.html', { tile: 'ConstructoraMA' });
 });
 
 //route view contact page
-router.get('/contact', (req,res) => {
-  res.render('contact.html',);
+router.get('/contact', (req, res) => {
+    res.render('contact.html');
 });
 
 //route view services page
-router.get('/services', (req,res) => {
-  res.render('services.html',);
+router.get('/services', (req, res) => {
+    res.render('services.html');
 });
-router.get('/trabajos', (req,res) => {
-  res.render('trabajos.html',);
+router.get('/trabajos', (req, res) => {
+    res.render('trabajos.html');
 });
 
 module.exports = router;
